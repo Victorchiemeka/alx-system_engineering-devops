5596897b91e3f31103def4de432ea8cf506ed678
Victorchiemeka <victorchukwuemeka127@gmail.com>
2023-05-23 12:13:12 +0100

    a program that prints _putchar, followed by a new line.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
- * main - a function that prints out string 
+ * main - a function that prints out string
  * Return: always sucess
  */
 int main(void)
 {
 	char string[] = "_putchar";
 	int index;
-	
-	for(index = 0; string[index]; index++)
+
+	for (index = 0; string[index]; index++)
 		putchar(string[index]);
 	putchar('\n');
+	return (0);
 }
